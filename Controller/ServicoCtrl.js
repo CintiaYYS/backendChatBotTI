9cd412554b80bd68927a13fc12be1907112347e6
CintiaYYS <cintiayys@gmail.com>
2024-10-19 16:51:43 -0300

    concluindo a camada de controle

--- Controller/ServicoCtrl.js
+++ Controller/ServicoCtrl.js
@@ -5,7 +5,7 @@ export default class ServicoCtrl{
     //Considerar o protocolo HTTP
 
     gravar(requisicao, resposta){
-        if (requisicao.method = "POST" && requisicao.is("application/json")){
+        if (requisicao.method == "POST" && requisicao.is("application/json")){
             const dados = requisicao.body;
             //Pseudo validação
             if(dados.nome && dados.descricao && dados.valor>=0 && dados.urlImagem && 
@@ -36,11 +36,83 @@ export default class ServicoCtrl{
     }
 
     alterar(requisicao, resposta){
+        if ((requisicao.method == "PUT" || requisicao.method == "PATCH") && requisicao.is("application/json")){
+            const dados = requisicao.body;
+            //Pseudo validação
+            if(dados.id >0 && dados.nome && dados.descricao && dados.valor>=0 && dados.urlImagem && 
+                dados.tempoInicioAtendimento>0 && dados.tempoSolucao>0){
+                    const servico = new Servico(dados.id,dados.nome, dados.descricao,dados.valor,
+                        dados.urlImagem,dados.tempoInicioAtendimento,dados.tempoSolucao);
+                    
+                    servico.alterar().then(()=>{                                        
+                        resposta.status(200).json({
+                                    "status":true,
+                                    "mensagem":"Serviço alterar com sucesso!",
+                        });
+                    }).catch((erro)=>{                        
+                        resposta.status(400).json({
+                                "status":false,
+                                "mensagem":"Erro ao alterar o serviço!"+erro.message
+                        })
+                    })
+            }
+            else{
+                resposta.status(405).json({
+                    "status":false,
+                    "mensagem":"Formato não permitido!"
+                })
+            }
+        }
     }
 
     excluir(requisicao, resposta){
+        if (requisicao.method == "DELETE" && requisicao.is("application/json")){
+            const id = requisicao.params.id;//id deve ser informado na url
+            //Pseudo validação
+            if(dados.id > 0){
+                    const servico = new Servico(id);
+                    
+                    servico.excluir().then(()=>{                                        
+                        resposta.status(200).json({
+                                    "status":true,
+                                    "mensagem":"Serviço EXCLUÍDO com sucesso!",
+                        });
+                    }).catch((erro)=>{                        
+                        resposta.status(500).json({
+                                "status":false,
+                                "mensagem":"Erro ao excluir o serviço!"+erro.message
+                        })
+                    })
+            }
+            else{
+                resposta.status(400).json({
+                    "status":false,
+                    "mensagem":"Formato não permitido!"
+                })
+            }
+        }
     }
 
     consultar(requisicao, resposta){
-    }
+        if (requisicao.method == "GET"){
+            const servico = new Servico(0);
+            servico.consultar().then((listaServicos)=>{
+                respostas.status(200).json({
+                    "status":true,
+                    "listaServicos":listaServicos
+                });
+            }).catch((erro)=>{                        
+                        resposta.status(500).json({
+                                "status":false,
+                                "mensagem":"Não foi possível carregar os serviços!"+erro.message
+                        })
+                    })
+            }
+            else{
+                resposta.status(405).json({
+                    "status": false,
+                    "mensagem":"Método não poermitido"
+                })
+            }     
+        }
 }
